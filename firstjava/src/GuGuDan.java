ab081dde05b282c083b4c832f0aaa1e530ab01aa
leejaewon23 <sosekee1110@naver.com>
2026-01-14 16:43:21 +0900

    test

--- firstjava/src/GuGuDan.java
+++ firstjava/src/GuGuDan.java
@@ -1,12 +1,23 @@
 public class GuGuDan {
     public static void main(String[] args) {
 
-        for (int i = 2; i <= 9; i++) {
-            System.out.println("*** " + i + "단 ***");
+        for (int i = 2; i < 10; i += 3) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 3; k++) {
+                    int dan = i + k;
+                    if (dan > 9) break; // 10단 이상 출력 방지
 
-            for (int k = 1; k <= 9; k++) {
-                System.out.println(i + "x" + k + "=" + i * k);
+                    if (j == 0) {
+                        System.out.print("  " + dan + "단\t\t"); // 탭으로 간격 조정
+                    } else {
+                        // 문자열을 더해서 출력
+                        System.out.print(dan + " * " + j + " = " + (dan * j) + "\t");
+                    }
+                }
+                System.out.println();
             }
+            System.out.println(); // 단 묶음 사이 줄바꿈
+        }
         }
     }
-}
+
--- firstjava/src/JavaVersion.java
+++ firstjava/src/JavaVersion.java
@@ -3,6 +3,6 @@ public class JavaVersion {
         // 자바 버전 구하기
         String version = System.getProperty("java.version");
 
-        System.out.println("My Java Version: " + version);
+        System.out.println("My Java Version= " + version);
     }
 }
